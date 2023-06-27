86a6c735f428895e42fdb0bcc4d8d3cd231972d9
anubhav100rao <anubhav100rao@gmail.com>
2023-06-27 21:07:49 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# virtualizations
+
+# cpu, memory, disk
+
+operating system creates illusion of infinite resource<br>
+this repo contains some situations describing that
---
+++ virtualCpu/cpu.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include "common.h"
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        fprintf(stderr, "Usage: mem <value>\n");
+        exit(1);
+    }
+
+    char* str = argv[1];
+
+    while(1) {
+        printf("%s\n", str);
+        Spin(1);
+    }
+}
+
+// run with ./cpu A & ./cpu B & ./cpu C & ./cpu D &
+// to kill these processes
+// open shell
+// ps -aux | grep ./cpu
+// kill all pids
